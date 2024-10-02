4645df5649c060b5cfb73e09fdc552895344107c
Jamil-4khtar <jamil4khtar@gmail.com>
2024-10-02 19:38:54 +0530

    created a submit

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
 //your code here
+// let divArr = ["div1", "div2", "div3", "div4", "div5", "div6"];
+
+let images = document.querySelectorAll(".image");
+
+// let c = 1;
+images.forEach(img => {
+    // img.id = "div"+c
+    // c++
+    img.addEventListener("dragstart", dragStart)
+    img.addEventListener("dragover", dragOver)
+    img.addEventListener("drop", drop)
+    // console.log(img.style);
+    
+});
+
+
+function dragStart(e) {
+    e.dataTransfer.setData("text", e.target.id);
+}
+function dragOver(e) {
+    e.preventDefault()
+}
+
+function drop(e) {
+    e.preventDefault();
+    let draggedDivId = e.dataTransfer.getData('text');
+    let draggedDiv = document.getElementById(draggedDivId);
+    let droppedOnDiv = e.target;
+    
+    // Swap backgroundImage
+    let temp = draggedDiv.style.backgroundImage;
+    draggedDiv.style.backgroundImage = droppedOnDiv.style.backgroundImage;
+    droppedOnDiv.style.backgroundImage = temp;
+}
